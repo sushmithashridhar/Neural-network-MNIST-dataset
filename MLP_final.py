1899718e2f3241609a105b741b174860b406392b
Sushmitha Shridhar <shridhar@pdx.edu>
2018-05-03 12:13:38 -0700

    Changes to variables to make it readable

--- MLP_final.py
+++ MLP_final.py
@@ -4,7 +4,12 @@ from sklearn.metrics import confusion_matrix
 
 Learning_rate = 0.1
 Momentum = 0.9
+Hidden_Layer_1 = 20
+Hidden_Layer_2 = 50
+Hidden_Layer_3 = 100
 
+
+########################################################## TRAIN ################################################################
 f = gzip.open("train-images-idx3-ubyte.gz", 'rb')
 train_images = np.frombuffer(f.read(), np.uint8, offset=16)
 train_images = train_images.reshape(-1, 1, 28, 28)
@@ -22,17 +27,7 @@ for row in range(len(train_labels)):
     index = train_labels[row].astype(int)
     train_labels_final[row][index[0]] += 0.8
 
-weights2 = np.random.uniform(-0.05, 0.05, size=(785,20))
-deltaO = np.zeros((21,10))
-deltaH = np.zeros((20,785))
-weights3 = np.random.uniform(-0.05, 0.05, size=(21,10))
-accuracy = 0
-h6 = np.zeros((60000,1))
-finding_accuracy_train = np.zeros((60000,1))
-finding_accuracy_test = np.zeros((10000,1))
-h3_with_bias = np.zeros(21)
-h3_with_bias[0] = 1
-
+############################################################# TEST ################################################################
 
 f = gzip.open("t10k-images-idx3-ubyte.gz", 'rb')
 test_images = np.frombuffer(f.read(), np.uint8, offset=16)
@@ -51,50 +46,67 @@ for row in range(len(test_labels)):
     index = test_labels[row].astype(int)
     test_labels_final[row][index[0]] += 0.8
 
-for i in range(50):
+####################################################################################################################################
+
+hidden_layer_weights = np.random.uniform(-0.05, 0.05, size=(785,Hidden_Layer_1))
+output_layer_weights_with_bias = np.random.uniform(-0.05, 0.05, size=(Hidden_Layer_1+1,10))
+
+modified_weights_hidden_to_output = np.zeros((Hidden_Layer_1+1,10))
+modified_weights_input_to_hidden = np.zeros((Hidden_Layer_1,785))
+
+
+h6 = np.zeros((60000,1))
+finding_accuracy_train = np.zeros((60000,1))
+finding_accuracy_test = np.zeros((10000,1))
+hj = np.zeros(Hidden_Layer_1+1)
+hj[0] = 1
+
+
+
+for i in range(1):
     for row in range(len(train_images)):
-        h2 = np.dot(train_images[row][:],weights2)
+        wjixi = np.dot(train_images[row][:],hidden_layer_weights)
         
-        h3_with_bias[1:] = 1 / ( 1 + np.exp(-h2))
-        h4 = np.dot(h3_with_bias,weights3)
-        #print h4.shape
-        h5 = 1 / ( 1 + np.exp(-h4))
-        output_error = h5 * (1 - h5) * (train_labels_final[row][:] - h5) #1*10
+        hj[1:] = 1 / ( 1 + np.exp(-wjixi))
+        wkjhj = np.dot(hj,output_layer_weights_with_bias)
+        #print wkjhj.shape
+        Ok = 1 / ( 1 + np.exp(-wkjhj))
+        compute_error_at_output = Ok * (1 - Ok) * (train_labels_final[row][:] - Ok) #1*10
 
 
-        x = h3_with_bias[1:] * ( 1 - h3_with_bias[1:] )
-        y =  np.dot(weights3[1:,:],np.transpose(output_error))
-        hidden_error = x * y
-        #print "X.shape" , x.shape , "y.shape" , y.shape , "hiddem_error", hidden_error.shape
+        x = hj[1:] * ( 1 - hj[1:] )
+        y =  np.dot(output_layer_weights_with_bias[1:,:],np.transpose(compute_error_at_output))
+        compute_error_at_hiddenlayer_dj = x * y
+        #print "X.shape" , x.shape , "y.shape" , y.shape , "hiddem_error", compute_error_at_hiddenlayer_dj.shape
 
-        step_1 = Learning_rate * np.outer(h3_with_bias,output_error)
-        step_2 = Momentum * deltaO
-        newdeltaO = step_1 + step_2
+        step_1 = Learning_rate * np.outer(hj,compute_error_at_output)
+        step_2 = Momentum * modified_weights_hidden_to_output
+        newmodified_weights_hidden_to_output = step_1 + step_2
 
-        step_1 = Learning_rate * np.outer(hidden_error,train_images[row][:])
-        #print step_1.shape , hidden_error.shape ,train_images[row][:].shape
-        step_2 = Momentum * deltaH
-        newdeltaH = step_1 + step_2
+        step_1 = Learning_rate * np.outer(compute_error_at_hiddenlayer_dj,train_images[row][:])
+        #print step_1.shape , compute_error_at_hiddenlayer_dj.shape ,train_images[row][:].shape
+        step_2 = Momentum * modified_weights_input_to_hidden
+        newmodified_weights_input_to_hidden = step_1 + step_2
 
-        weights3 += newdeltaO
-        deltaO = newdeltaO
-        weights2 += np.transpose(newdeltaH)
-        deltaH = newdeltaH
+        output_layer_weights_with_bias += newmodified_weights_hidden_to_output
+        modified_weights_hidden_to_output = newmodified_weights_hidden_to_output
+        hidden_layer_weights += np.transpose(newmodified_weights_input_to_hidden)
+        modified_weights_input_to_hidden = newmodified_weights_input_to_hidden
 
-        index = np.argmax(h5,axis=0)
+        index = np.argmax(Ok,axis=0)
         finding_accuracy_train[row] =  index
 
 
     for row in range(len(test_images)):
         
-        h2 = np.dot(test_images[row][:],weights2)
+        wjixi = np.dot(test_images[row][:],hidden_layer_weights)
 
-        h3_with_bias[1:] = 1 / ( 1 + np.exp(-h2))
-        h4 = np.dot(h3_with_bias,weights3)
-        #print h4.shape
-        h5 = 1 / ( 1 + np.exp(-h4))
+        hj[1:] = 1 / ( 1 + np.exp(-wjixi))
+        wkjhj = np.dot(hj,output_layer_weights_with_bias)
+        #print wkjhj.shape
+        Ok = 1 / ( 1 + np.exp(-wkjhj))
 
-        index = np.argmax(h5,axis=0)
+        index = np.argmax(Ok,axis=0)
         finding_accuracy_test[row] =  index
 
 
